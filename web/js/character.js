860b7c378e5eff830c417e0c692bdf685454237d
Trevor Ceniviva <trevorceniviva@Trevors-MBP.fios-router.home>
2020-08-25 11:09:06 -0400

    done all subraces

--- web/js/character.js
+++ web/js/character.js
@@ -17,14 +17,17 @@ let player = {
 
 const selectedRace = document.getElementById("race-dropdown");
 const selectSubrace = document.getElementById("subrace-dropdown");
-// let subraceOptions = [];
+const selectClass = document.getElementById("first-class-dropdown");
+
 let levelsAvailable = 20;
 let abilityModsAvailable = 0;
 let countLanguagesAvailable = 0;
 let languagesAvailable = [];
 let allLanguages = ['Abyssal', 'Celestial', 'Common', 'Daelkyr', 'DeepSpeech', 'Draconic', 'Dwarvish', 'Elvish', 'Giant', 'Gith', 'Gnomish', 'Goblin', 'Halfling', 'Infernal', 'Kraul', 'Leonin', 'Loxodon', 'Marquesian', 'Minotaur', 'Naush', 'Orc', 'Primordial', 'Quori', 'Riedran', 'Sylvan', 'Undercommon', 'Vedalken', 'Zemnian']
 
-var playerRace
+var playerRace;
+var playerSubrace;
+var playerClass;
 
 let raceData = [
     {
@@ -355,6 +358,211 @@ let raceData = [
     }
 ]
 
+let subraceData = [
+    {
+        subrace: "Dark",
+        parentRace: "Elf",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 1,
+        speed: 0,
+    },
+    {
+        subrace: "Eladrin",
+        parentRace: "Elf",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 1,
+        speed: 0,
+    },
+    {
+        subrace: "High",
+        parentRace: "Elf",
+        languages: [],
+        countLangAdd: 1,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 1,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Wood",
+        parentRace: "Elf",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 1,
+        cha: 0,
+        speed: 5,
+    },
+    {
+        subrace: "Hill",
+        parentRace: "Dwarf",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 1,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Mountain",
+        parentRace: "Dwarf",
+        languages: [],
+        countLangAdd: 0,
+        str: 2,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+        
+    {
+        subrace: "Air",
+        parentRace: "Genasi",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 1,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Earth",
+        parentRace: "Genasi",
+        languages: [],
+        countLangAdd: 0,
+        str: 1,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Fire",
+        parentRace: "Genasi",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 1,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Water",
+        parentRace: "Genasi",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 1,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Forest",
+        parentRace: "Gnome",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 1,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Rock",
+        parentRace: "Gnome",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 1,
+        int: 0,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    },
+    {
+        subrace: "Lightfoot",
+        parentRace: "Halfling",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 0,
+        int: 0,
+        wis: 0,
+        cha: 1,
+        speed: 0,
+    },
+    {
+        subrace: "Stout",
+        parentRace: "Halfling",
+        languages: [],
+        countLangAdd: 0,
+        str: 0,
+        dex: 0,
+        con: 1,
+        int: 0,
+        wis: 0,
+        cha: 0,
+        speed: 0,
+    }
+]
+
+let classData = [
+    {class: "Artificer",
+    armor: ["Light Armor", "Medium Armor", "Shields"],
+    weapons: ["Simple Weapons"],
+    tools: ["Theives' Tools", "Tinker's Tools"],
+    countToolsAdd: 1,
+    toolOptions: [],
+    skills: [],
+    countSkillsAdd: 2,
+    skillsOptions: ["Arcana", "History", "Investigation", "Medicine", "Nature", "Perception", "Sleight of Hand"] 
+}
+]
+
+function setPlayerClass() {
+    for (i = 0; i < classData.length; i++){
+        if (classData[i].class === selectClass.value) {
+            playerClass = classData[i];}}
+        }
+
 function setPlayerRace() {
     for (i = 0; i < raceData.length; i++) {
         if (raceData[i].race === selectedRace.value) {
@@ -363,7 +571,7 @@ function setPlayerRace() {
     }
 }
 
-function showSubraceOptions(race) {
+function showSubraceOptions() {
     selectSubrace.innerHTML = "";
     if (playerRace.subraces.length > 0) {
         document.getElementById("subrace-container").classList.remove("hide-element");
@@ -375,3 +583,24 @@ function showSubraceOptions(race) {
     }
 }
 
+function setPlayerSubrace() {
+    for (i=0; i<subraceData.length;i++) {
+        if (subraceData[i].subrace === selectSubrace.value) {
+            playerSubrace = subraceData[i];
+        }
+    }
+}
+
+function applySkills() {
+    var skillChecks = document.querySelectorAll(".skill-box")
+    for (i=0; i<skillChecks.length; i++) {skillChecks[i].checked=false; skillChecks[i].disabled=true}
+
+
+
+    if (playerRace.skills.length > 0) {
+        for (i=0; i<playerRace.skills.length; i++) {
+            var skillBox = document.getElementById(playerRace.skills[i].toLowerCase());
+            skillBox.checked = true;
+        }
+    }
+}
